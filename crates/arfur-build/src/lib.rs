9a25c195ff87777b6867e3d605a23749552a980d
Bobbbay <abatterysingle@gmail.com>
2022-09-06 16:56:20 +0000

    feat(arfur-sys): simulation support

--- crates/arfur-build/src/lib.rs
+++ crates/arfur-build/src/lib.rs
@@ -163,7 +163,36 @@ impl<'a> Runner<'a> {
                 .join("HAL_Wrapper.h"),
         )?;
 
-        let data = "#include <hal/HAL.h>";
+        let data = r##"
+            #include <hal/HAL.h>
+            #include <hal/simulation/AccelerometerData.h>
+            #include <hal/simulation/AddressableLEDData.h>
+            #include <hal/simulation/AnalogGyroData.h>
+            #include <hal/simulation/AnalogInData.h>
+            #include <hal/simulation/AnalogOutData.h>
+            #include <hal/simulation/AnalogTriggerData.h>
+            #include <hal/simulation/CTREPCMData.h>
+            #include <hal/simulation/CanData.h>
+            #include <hal/simulation/DIOData.h>
+            #include <hal/simulation/DigitalPWMData.h>
+            #include <hal/simulation/DriverStationData.h>
+            #include <hal/simulation/DutyCycleData.h>
+            #include <hal/simulation/EncoderData.h>
+            #include <hal/simulation/I2CData.h>
+            #include <hal/simulation/MockHooks.h>
+            #include <hal/simulation/NotifierData.h>
+            #include <hal/simulation/NotifyListener.h>
+            #include <hal/simulation/PWMData.h>
+            #include <hal/simulation/PowerDistributionData.h>
+            #include <hal/simulation/REVPHData.h>
+            #include <hal/simulation/RelayData.h>
+            #include <hal/simulation/RoboRioData.h>
+            #include <hal/simulation/SPIAccelerometerData.h>
+            #include <hal/simulation/SPIData.h>
+            #include <hal/simulation/SimCallbackRegistry.h>
+            #include <hal/simulation/SimDataValue.h>
+            #include <hal/simulation/SimDeviceData.h>
+        "##;
 
         f.write_all(data.as_bytes())?;
 
