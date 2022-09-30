4ef435a0e6bc137f127a37fb9f41ddb3a6928b25
Bobbbay <abatterysingle@gmail.com>
2022-09-30 01:11:44 +0000

    fix(examples): initialize `ADXRS450_Gyro` just once

--- examples/raw_usage.rs
+++ examples/raw_usage.rs
@@ -6,14 +6,15 @@ use arfur::wpilib::ffi;
 fn main() -> Result<()> {
     let _robot = RobotBuilder::default().initialize();
 
-    loop {
-        unsafe {
-            let mut gyro = ffi::frc_ADXRS450_Gyro::new();
+    unsafe {
+        let mut gyro = ffi::frc_ADXRS450_Gyro::new();
+
+        loop {
             let angle =
                 ffi::frc_ADXRS450_Gyro_GetAngle(&mut gyro as *mut _ as *mut std::ffi::c_void);
             println!("Found angle to be {}", angle);
-        };
 
-        std::thread::sleep(std::time::Duration::from_secs(1));
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
     }
 }
