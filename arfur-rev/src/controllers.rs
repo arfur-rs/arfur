24655fca39f150b0e61256aed5150f37322d229f
Bobbbay <abatterysingle@gmail.com>
2022-11-05 17:24:13 +0000

    chore(arfur-rev): derive Debug on `SparkMax`

--- arfur-rev/src/controllers.rs
+++ arfur-rev/src/controllers.rs
@@ -6,6 +6,7 @@ pub mod sparkmax {
     use crate::ffi::root::rev::{CANSparkMax, CANSparkMax_Set};
 
     /// A handle to a REV CAN SparkMax motor controller.
+    #[derive(Debug)]
     pub struct SparkMax {
         handle: CANSparkMax,
     }
@@ -24,6 +25,7 @@ pub mod sparkmax {
         /// Set the percentage output.
         ///
         /// Safety: the percentage is a number from -1 to 1.
+        #[tracing::instrument]
         pub fn set_percentage(&mut self, percentage: f64) {
             unsafe {
                 CANSparkMax_Set(
